53db62a72aff7d1522f0e43143c904b47c4396b2
Emily Graber <36776355+emgraber@users.noreply.github.com>
2023-10-18 17:03:47 -0400

    Initial commit

---
+++ palindromechecker/palindromechecker/palindrome.py
@@ -0,0 +1,15 @@
+"""Determine whether or not an input string is a palindrome."""
+
+# Reference:
+# https://en.wikipedia.org/wiki/Palindrome
+
+# TODO: implement to_chars(word: str) -> str:
+
+# TODO: implement def is_palindrome(word: str) -> bool:
+
+# TODO: implement def is_palindrome_recursive(word: str) -> bool:
+
+# TODO: implement def is_palindrome_reverse(word: str) -> bool:
+
+# TODO: make sure to all all of the required functions so as to ensure
+# that the test cases in the test_palindrome.py file all pass correctly
---
+++ palindromechecker/palindromechecker/util.py
@@ -0,0 +1,3 @@
+# TODO: Add the required docstring and any required imports of objects
+
+# TODO: implement the def get_human_readable_boolean(answer: bool) -> str function
